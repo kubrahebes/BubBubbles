fc99c987ddc8c2cc02d5d3dcaceaaf6431d4b604
kubra.hebes@gmail.com <kubra.hebes@gmail.com>
2018-03-03 15:11:48 +0200

    ana yapi olusturuldu

--- app/src/main/java/com/example/user/bubbubbles/SpleshActivity.java
+++ app/src/main/java/com/example/user/bubbubbles/SpleshActivity.java
@@ -3,11 +3,11 @@ package com.example.user.bubbubbles;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SpleshActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splesh);
     }
 }
